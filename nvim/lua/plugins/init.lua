cd9ca7e16cb77daacd3a05fcc340c689edab8cba
Carlo Dacuyan <cdacuyan@ubx.ph>
2024-06-04 13:55:35 +0800

    first commit

---
+++ nvim/lua/plugins/init.lua
@@ -0,0 +1,77 @@
+return {
+  -- Navigation Plugins
+  {
+    "christoomey/vim-tmux-navigator",
+    lazy = false,
+  },
+
+  -- Formatting Plugins
+  {
+    "stevearc/conform.nvim",
+    event = "BufWritePre",
+    config = function()
+      require "configs.conform"
+    end,
+  },
+
+  -- LSP Configuration
+  {
+    "neovim/nvim-lspconfig",
+    config = function()
+      require("nvchad.configs.lspconfig").defaults()
+      require "configs.lspconfig"
+    end,
+  },
+  {
+    "williamboman/mason.nvim",
+    opts = {
+      ensure_installed = {
+        "lua-language-server",
+        "stylua",
+        "html-lsp",
+        "css-lsp",
+        "prettierd",
+        "typescript-language-server",
+        "pyright",
+        "tailwindcss-language-server",
+        "eslint_d",
+        "pylint",
+      },
+    },
+  },
+
+  -- Treesitter Configuration
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "vim",
+        "lua",
+        "vimdoc",
+        "html",
+        "css",
+        "javascript",
+        "typescript",
+      },
+    },
+  },
+
+  -- Language Specific Plugins
+  {
+    "maxmellon/vim-jsx-pretty",
+    ft = { "javascriptreact", "typescriptreact" },
+  },
+  {
+    "mattn/emmet-vim", -- Optional: Added for Emmet support in HTML and JSX/TSX
+    ft = { "html", "javascriptreact", "typescriptreact" },
+  },
+
+  -- Linting Configuration
+  {
+    "mfussenegger/nvim-lint",
+    event = { "BufReadPre", "BufNewFile" },
+    config = function()
+      require "configs.lint"
+    end,
+  },
+}
