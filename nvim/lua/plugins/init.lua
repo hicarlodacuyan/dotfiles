efd7b1ba489f049d62b1f49e4724d48c39963a70
Carlo Dacuyan <cdacuyan@ubx.ph>
2024-06-25 15:38:08 +0800

    Remove codeium

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -74,28 +74,4 @@ return {
       require "configs.lint"
     end,
   },
-
-  -- Codeium
-  {
-    "Exafunction/codeium.vim",
-    event = "BufEnter",
-    config = function()
-      vim.g.codeium_disable_bindings = 1
-      vim.keymap.set("i", "<C-g>", function()
-        return vim.fn["codeium#Accept"]()
-      end, { expr = true, silent = true })
-
-      vim.keymap.set("i", "<c-n>", function()
-        return vim.fn["codeium#CycleCompletions"](1)
-      end, { expr = true, silent = true })
-
-      vim.keymap.set("i", "<c-p>", function()
-        return vim.fn["codeium#CycleCompletions"](-1)
-      end, { expr = true, silent = true })
-
-      vim.keymap.set("i", "<c-x>", function()
-        return vim.fn["codeium#Clear"]()
-      end, { expr = true, silent = true })
-    end,
-  },
 }
